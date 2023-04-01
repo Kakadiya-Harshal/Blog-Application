454bb67c601867e8a7585c8c5ae447b2fd3d3e52
Kakadiya-Harshal <113292495+Kakadiya-Harshal@users.noreply.github.com>
2023-04-01 18:19:15 +0530

    Initialized frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
-
-// import blogRouter from "./routes/blog-routes";
-// import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes.js";
+import router from "./routes/user-routes.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/databaseConnection.js";
@@ -14,8 +13,8 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use("/api/user", router);
-// app.use("/api/blog", blogRouter);
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
