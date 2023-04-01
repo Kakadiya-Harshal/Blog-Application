e64734b93a347933237f864f1259d5f83f5cad9f
Kakadiya-Harshal <113292495+Kakadiya-Harshal@users.noreply.github.com>
2023-04-01 16:50:47 +0530

    Added routes

---
+++ backend/config/databaseConnection.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB connected: ${conn.connection.host}`)
+    
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+export default connectDB
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+// import blogRouter from "./routes/blog-routes";
+// import router from "./routes/user-routes";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/databaseConnection.js";
+
+dotenv.config();
+connectDB();
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use("/api/user", router);
+// app.use("/api/blog", blogRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
